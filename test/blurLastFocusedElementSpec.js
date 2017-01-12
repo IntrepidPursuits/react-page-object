08fa347d6497fe83e78e061b27716b69205d674d
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-01-11 23:34:21 -0500

    rename blurLastTouchedElement -> blurLastFocusedElement

--- test/blurLastFocusedElementSpec.js
+++ test/blurLastFocusedElementSpec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Page from 'react-page-object'
 
-describe('blurLastTouchedElementSpec', () => {
+describe('blurLastFocusedElementSpec', () => {
   let page
 
   beforeEach(() => {
@@ -22,23 +22,23 @@ describe('blurLastTouchedElementSpec', () => {
 
     it('should simulate a \'blur\' event on the previously focused wrapper', () => {
       window.spyOn(fakeWrapper, 'simulate')
-      page.blurLastTouchedElement()
+      page.blurLastFocusedElement()
       expect(fakeWrapper.simulate).toHaveBeenCalledWith('blur')
     })
 
     it('should set previouslyFocusedWrapper to null', () => {
-      page.blurLastTouchedElement()
+      page.blurLastFocusedElement()
       expect(page.previouslyFocusedWrapper).toEqual(null)
     })
 
     it('should return the page object itself', () => {
-      expect(page.blurLastTouchedElement()).toBe(page)
+      expect(page.blurLastFocusedElement()).toBe(page)
     })
   })
 
   describe('there is no previously focused wrapper', () => {
     it('should not throw an error if method is invoked', () => {
-      expect(() => page.blurLastTouchedElement()).not.toThrow()
+      expect(() => page.blurLastFocusedElement()).not.toThrow()
     })
   })
 })
